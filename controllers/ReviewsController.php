88d7ba826f3f52b75ca15abd95c09820141079bc
Zachary Seguin <development@zacharyseguin.ca>
2015-01-25 15:43:44 -0500

    Works better when it is a JSON response

--- controllers/ReviewsController.php
+++ controllers/ReviewsController.php
@@ -73,7 +73,7 @@ class ReviewsController
       $res = $this->db->addOutletReview($outlet_id, $data['name'], $data['email'], $data['review'], $request->getClientIp());
 
       if ($res !== FALSE) return new Response("");
-      return new Response(array("error" => "Sorry, an unexpected error ocurred."), 500);
+      return new JsonResponse(array("error" => "Sorry, an unexpected error ocurred."), 500);
    }// End of addOutletReviewAction method
 }// End of ReviewsController class
 
